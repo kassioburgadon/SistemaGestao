64c9f9e1665d4498ea37e093317fe1bdc00ba4d7
Kassio Burgadon <kassioburgadon@gmail.com>
2026-05-14 07:50:03 -0300

    Controller MVC Utilizando o Entity Framework

---
+++ SistemaGestao/Data/SistemaGestaoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaGestao;
+
+namespace SistemaGestao.Data
+{
+    public class SistemaGestaoContext : DbContext
+    {
+        public SistemaGestaoContext (DbContextOptions<SistemaGestaoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaGestao.Tarefa> Tarefa { get; set; } = default!;
+    }
+}
