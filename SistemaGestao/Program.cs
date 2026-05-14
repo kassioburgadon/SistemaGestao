e84e1b7bc042af8fe99e0c1670f6b4688219ddfc
Kassio Burgadon <kassioburgadon@gmail.com>
2026-05-14 09:44:24 -0300

    Ajustar o Enum para Deixar com as opções da palavra

--- SistemaGestao/Program.cs
+++ SistemaGestao/Program.cs
@@ -1,13 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SistemaGestao.Data;
+using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SistemaGestaoContext>(options =>
     options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("SistemaGestaoContext") ?? throw new InvalidOperationException("Connection string 'SistemaGestaoContext' not found.")));
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
